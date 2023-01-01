4333787b3772e5f89596a9397a1c5def7198c5a8
Saman462 <szohra.bese-26mcs@student.nust.edu.pk>
2023-01-01 16:16:56 +0500

    middleware added

--- pages/About.js
+++ pages/About.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function About(props) {
-    return (
-        <div>
-            <p> About Page </p>
-        </div>
-    );
+  return (
+    <div>
+      <p> About Page </p>
+    </div>
+  );
 }
 
 export default About;
